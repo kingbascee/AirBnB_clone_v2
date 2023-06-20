cc573f1d69d5b6ae6ca10d7d94a92ccfe9171916
Eyosiyas A Gudeta <realgaz.eys@gmail.com>
2023-06-20 16:56:20 +0000

    update model files

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module for Review class"""
+"""This is the review class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel, Base):
-    """This is for the class for Review
+    """This is the class for Review
     Attributes:
         place_id: place id
         user_id: user id
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for User class."""
+"""This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -9,7 +9,7 @@ from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This is the class reprensentation for user
+    """This is the class for user
     Attributes:
         email: email address
         password: password for you login
